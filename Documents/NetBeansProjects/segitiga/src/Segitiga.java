b28f5ea4c31df4a1d1b52052895c55b7bb27e370
sigit23 <pri.yant0@ovi.com>
2016-10-26 21:44:50 +0700

    12141384: cek

---
+++ Documents/NetBeansProjects/segitiga/src/Segitiga.java
@@ -0,0 +1,16 @@
+public class Segitiga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        for (int i = 0; i < 6; i++) {
+            for (int k = 0; k < i; k++) {
+                System.out.print("s");
+            }
+            System.out.println();
+        }
+    }
+}
